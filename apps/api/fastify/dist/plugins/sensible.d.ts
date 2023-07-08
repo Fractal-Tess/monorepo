694885ec1a9986f9ca8b5fad9f9894cfef28ae87
Fractal-Tess <vgfractal@gmail.com>
2023-07-08 22:40:25 +0300

    fastify

---
+++ apps/api/fastify/dist/plugins/sensible.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="node" />
+import { SensibleOptions } from '@fastify/sensible';
+declare const _default: import("fastify").FastifyPluginAsync<SensibleOptions, import("http").Server, import("fastify").FastifyTypeProviderDefault>;
+export default _default;
