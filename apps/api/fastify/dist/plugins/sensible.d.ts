09a0a7996bede05d4da74622b5e2c814f4ae55ce
Fractal-Tess <vgfractal@gmail.com>
2023-07-20 01:18:09 +0300

    remove fastify

--- apps/api/fastify/dist/plugins/sensible.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import { SensibleOptions } from '@fastify/sensible';
-declare const _default: import("fastify").FastifyPluginAsync<SensibleOptions, import("http").Server, import("fastify").FastifyTypeProviderDefault>;
-export default _default;
