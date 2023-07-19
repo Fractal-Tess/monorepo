e8de0620ac41855065ead8ff4430a8e56f4d04cb
Fractal-Tess <vgfractal@gmail.com>
2023-07-19 23:26:03 +0300

    svelte starter

--- apps/typescript/src/main.ts
+++ apps/typescript/src/main.ts
@@ -1,6 +1,10 @@
 import { ENV } from './env.js';
+import type { Bar } from './types.js';
 
+const eggs: Bar = { foo: 'Stuff' };
+
+console.log('Program entry');
 await new Promise(res => setTimeout(res, 1000));
-console.log('Await works');
+console.log('Top-level await works');
 
 console.log(`ENV.FOO is ${ENV.BAR}`);
---
+++ apps/typescript/src/types.ts
@@ -0,0 +1,1 @@
+export type Bar = { foo: string };
