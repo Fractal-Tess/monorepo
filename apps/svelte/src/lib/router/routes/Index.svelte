21b240e6e7a0eb4776b1fec5627c3f0cdb23ed9c
Fractal-Tess <vgfractal@gmail.com>
2023-07-23 01:31:24 +0300

    free package inherits logic

--- apps/svelte/src/lib/router/routes/Index.svelte
+++ apps/svelte/src/lib/router/routes/Index.svelte
@@ -2,7 +2,9 @@
 </script>
 
 <div
-  class="form-control flex-1 items-center text-black dark:text-white justify-center space-y-6 text-center font-bold">
+  class="form-control relative flex-1 items-center justify-center space-y-6 text-center font-bold text-black dark:text-white">
+  <div
+    class="bg-base-content blurxl absolute left-1/3 top-1/3 h-40 w-40 border-2" />
   <h1 class="text-6xl">Welcome</h1>
   <h2 class="flex items-center text-3xl">
     This is an &#160
@@ -11,7 +13,7 @@
     </span>
     &#160
     <span
-      class="bg-clip-text text-transparent bg-gradient-to-b from-accent to-primary font-bold uppercase">
+      class="from-accent to-primary bg-gradient-to-b bg-clip-text font-bold uppercase text-transparent">
       spa
     </span> &#160 template
   </h2>
--- tools/package-inherit/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { updateCommand } from './updateCommand';
-import { checkCommand } from './checkCommand';
-
-export { updateCommand, checkCommand };
