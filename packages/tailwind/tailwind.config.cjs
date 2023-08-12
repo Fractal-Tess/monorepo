fc6ab21b786afb5e685bcdd2abe89f5c9b301f8d
Fractal-Tess <vgfractal@gmail.com>
2023-08-12 05:55:52 +0300

    update

--- packages/tailwind/tailwind.config.cjs
+++ packages/tailwind/tailwind.config.cjs
@@ -1,58 +1,59 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss-hero-patterns/src/patterns')} */
-const heropatterns = require('tailwindcss-hero-patterns/src/patterns')
+const heropatterns = require('tailwindcss-hero-patterns/src/patterns');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // IMPORTANT:
-  // Every package that wants to use this tailwind config
-  // needs to define the `content` property and tell tailwind
-  // where to watch for classes
-
   darkMode: 'class',
-  mode: 'jit',
+  content: ['../../packages/ui/components/**/*.{ts,tsx}'],
 
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px'
+      }
+    },
     extend: {
       fontFamily: {
         sans: ['roboto', ...defaultTheme.fontFamily.sans]
       },
 
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))'
+      heroPatternsOpacities: ['10'],
+      heroPatterns: {
+        topography: heropatterns.topography
       }
-    },
-    heroPatternsOpacities: ['10'],
-    heroPatterns: {
-      topography: heropatterns.topography
     }
   },
-
   daisyui: {
     themes: [
       {
         light: {
           ...require('daisyui/src/theming/themes')['[data-theme=light]'],
           primary: '#45B1E8',
-          secondary: '#E8456B',
-          accent: '#3AE8C4',
-          '--btn-text-case': 'none'
+          secondary: '#fbbf24',
+          error: '#ec223d'
         },
         dark: {
-          ...require('daisyui/src/theming/themes')['[data-theme=black]'],
+          ...require('daisyui/src/theming/themes')['[data-theme=dark]'],
           primary: '#45B1E8',
-          secondary: '#E8456B',
-          accent: '#3AE8C4',
-          '--btn-text-case': 'none',
-          '--rounded-box': '0.2rem',
-          '--rounded-btn': '0.2rem'
+          secondary: '#fbbf24',
+          error: '#ec223d'
         }
       }
-    ]
+    ],
+    // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: 'dark', // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true // Shows info about daisyUI version and used config in the console when building your CSS
   },
-
   plugins: [
+    require('tailwindcss-animate'),
     require('daisyui'),
     require('@tailwindcss/line-clamp'),
     require('tailwindcss-gradients'),
