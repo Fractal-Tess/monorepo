21ef16f19cf7e9e4d1df42cdc45b4ceb66c25a2c
Fractal-Tess <vgfractal@gmail.com>
2024-04-21 04:32:55 +0300

    remove old web-extension template

--- apps/web-extension/src/pages/content/Sample.tsx
+++
@@ -1,7 +0,0 @@
-export function Sample() {
-  return (
-    <p className="text-primary bg-base-300 rounded-box p-2">
-      Poly Starter content script
-    </p>
-  );
-}
--- apps/web-extension/src/pages/devtools/index.ts
+++
@@ -1,5 +0,0 @@
-import Browser from 'webextension-polyfill';
-
-Browser.devtools.panels
-  .create('Poly Starter Dev Tools', 'icon128.png', 'src/pages/panel/index.html')
-  .catch(console.error);
